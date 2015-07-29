corrected mistake in calculation of illumination plot

elements_vel_prism.py
@@ -398,7 +398,9 @@ def illum_track(data_x, input, profile, times, planet_absorb, spot_data=False, t
 
   wvl = profile['wvl']
 
-  vel_model = 2.3 + planet_K*sin(2.0*pi*(midtransit-times)/period)
+  star_rv = 2.1
+
+  vel_model = star_rv + 1*planet_K*sin(2.0*pi*(midtransit-times)/period)
   star_vel_model = -1*star_K*sin(2.0*pi*(midtransit-times)/period)
 
 
@@ -422,7 +424,7 @@ def illum_track(data_x, input, profile, times, planet_absorb, spot_data=False, t
   d1 = 5895.924
   d2 = 5889.950
 
-  c = 3e5
+  c = 299792.458
 
   shifted_d1 = d1*(1.0 - (vel_model/c))
   shifted_d2 = d2*(1.0 - (vel_model/c))
@@ -430,16 +432,18 @@ def illum_track(data_x, input, profile, times, planet_absorb, spot_data=False, t
   background_d1 = []
   background_d2 = []
 
+  star_vel = vel_grid[int_xx,planet_y]
+
   for i in range(0,len(vel_model)):
     ld = grid[xx[i],planet_y]
-    med_shifted_spectra = ld*shifted_spectra[i]/np.median(shifted_spectra[0])
+    med_shifted_spectra = ld*shifted_spectra[xx[i]]/np.median(shifted_spectra[0])
     background_d1 += [med_shifted_spectra[argmin(abs(wvl-shifted_d1[i]))]]
     background_d2 += [med_shifted_spectra[argmin(abs(wvl-shifted_d2[i]))]]
 
   background_d2 = np.array(background_d2)
   background_d1 = np.array(background_d1)
 
-  return wvl, background_d1, background_d2
+  return wvl, background_d1, background_d2, star_vel, vel_model-star_rv, shifted_d1,shifted_spectra[int_xx]
 
 def integrate_star_profile(scale,grid,profile,vel_grid,nproc=4):
   